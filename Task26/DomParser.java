426654f8081e6398fcd8cf4a9ef8781c4cc0ec92
dashaklimova <dasha.7777777@mail.ru>
2019-03-26 23:42:19 +0300

    added task26

---
+++ Task26/DomParser.java
@@ -0,0 +1,41 @@
+package dom;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import model.Country;
+
+public class DomParser {
+	public List<Country> parse(Document document) throws FileNotFoundException, XMLStreamException {
+		NodeList nodeList = document.getElementsByTagName("Country");
+		List<Country> сountries = new ArrayList<Country>();
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			сountries.add(getCountry(nodeList.item(i)));
+		}
+		return сountries;
+	}
+
+	private static Country getCountry(Node node) {
+		Country country = new Country();
+		Element element = (Element) node;
+		country.setId(Integer.parseInt(element.getAttribute("id")));
+		country.setCode(getTagValue("Code", element));
+		country.setName(getTagValue("Name", element));
+		country.setDescription(getTagValue("Description", element));
+		return country;
+	}
+
+	private static String getTagValue(String tag, Element element) {
+		NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
+		Node node = nodeList.item(0);
+		return node.getNodeValue();
+	}
+}
---
+++ Task26/Run.java
@@ -0,0 +1,53 @@
+package runner;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import dom.DomParser;
+import model.Country;
+import sax.CountryHandler;
+import stax.StaxParser;
+
+public class Run {
+
+	private static final String COUNTRIES_XML = "countries.xml";
+
+	public static void main(String[] args)
+			throws ParserConfigurationException, SAXException, IOException, XMLStreamException {
+		System.out.println(" ========================= SAX parser ==============================");
+		SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+		SAXParser saxParser = saxParserFactory.newSAXParser();
+		CountryHandler countryHandler = new CountryHandler();
+		saxParser.parse(new File(COUNTRIES_XML), countryHandler);
+		List<Country> сountries = countryHandler.getСountries();
+		сountries.forEach(country -> System.out.println(country));
+
+		System.out.println(" ============================== STAX parser =========================");
+		XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+		XMLEventReader xmlEventReader = xmlInputFactory.createXMLEventReader(new FileInputStream(COUNTRIES_XML));
+		сountries = new StaxParser().parse(xmlEventReader);
+		сountries.forEach(country -> System.out.println(country));
+
+		System.out.println(" ============================== DOM parser =========================");
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document document = dBuilder.parse(COUNTRIES_XML);
+		сountries = new DomParser().parse(document);
+		сountries.forEach(country -> System.out.println(country));
+	}
+
+}
